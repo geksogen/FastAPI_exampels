3d1144c076c61094ecd95a7575ae496607a84c5e
test <test@mail.com>
2021-07-21 04:17:11 -0700

    fixed issues in autorisation

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
 
 @app.get("/")
 def read_root(credentials: HTTPBasicCredentials = Depends(get_current_username)):
-    return {"Hello": credentials.username}
+    return {"Aloha": "Men :)"}
 
 @app.get("/test/{item_id}")
 async def read_item(item_id: int, credentials: HTTPBasicCredentials = Depends(get_current_username)):
