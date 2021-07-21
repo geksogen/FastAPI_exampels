89e0eb86971a8bd8d0bf80f0732121bbbd814ae0
test <test@mail.com>
2021-07-21 04:07:48 -0700

    add autorisations basic

--- app.py
+++ app.py
@@ -2,32 +2,47 @@ from fastapi import FastAPI
 import uvicorn
 from typing import Optional
 from pydantic import BaseModel
+import secrets
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 
 class Item(BaseModel):
     name: str
     description: Optional[str] = None
-    price: float
+    price: int
     tax: Optional[float] = None
 
 
 app = FastAPI()
 
+security = HTTPBasic()
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "otus")
+    correct_password = secrets.compare_digest(credentials.password, "otus123")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
+def read_root(credentials: HTTPBasicCredentials = Depends(get_current_username)):
+    return {"Hello": credentials.username}
 
 @app.get("/test/{item_id}")
-async def read_item(item_id: int):
+async def read_item(item_id: int, credentials: HTTPBasicCredentials = Depends(get_current_username)):
     print(item_id)
     return {"item_id": item_id}
 
 
 @app.post("/items/")
-async def create_item(item: Item):
+async def create_item(item: Item, credentials: HTTPBasicCredentials = Depends(get_current_username)):
     return item
-
 
 #uvicorn.run(app, host="0.0.0.0", port="8080")
